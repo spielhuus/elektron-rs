e19b3c9f09d6d4fe28652b541b6f24c1d6ed6edd
spielhuus <spielhuus@gmail.com>
2022-07-27 19:08:39 +0200

    display

--- python/elektron/__init__.py
+++ python/elektron/__init__.py
@@ -215,7 +215,7 @@ class Draw():
             write_chunked(a='T', f=100, data=bytearray(image))
         elif filename is None:
             image = self.schema.plot(filename, border, scale, "svg")
-            return HTML(data=bytearray(image))
+            return "".join(image)
         else:
             filetype = ""
             if filename.endswith(".png"):
